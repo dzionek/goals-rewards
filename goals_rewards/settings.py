5411792f1e9f1e418b4ed120499d1ea0ad3f2c20
dzionek <vburtoch@gmail.com>
2020-08-08 19:48:26 +0200

    Add account and system apps

--- goals_rewards/settings.py
+++ goals_rewards/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'system',
+    'account',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
