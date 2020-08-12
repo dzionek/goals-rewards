fe8eaabba668d15cb70295cc41d70a0bdd2ab7bf
dzionek <vburtoch@gmail.com>
2020-08-12 19:57:17 +0200

    Fix Travis

--- system/admin.py
+++ system/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import DirectReward, PointReward, Goal
+from .models import DirectReward, PointReward, Goal, UserPoint
 
 
 admin.site.register(DirectReward)
 admin.site.register(PointReward)
 admin.site.register(Goal)
+admin.site.register(UserPoint)
